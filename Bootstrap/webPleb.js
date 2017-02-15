0a4f26b155e54aa4b5f5345caa314d47343b75c9
markPoyhonen <mark.poyhonen@promessinc.com>
2017-02-15 15:48:15 -0500

    Add files via upload

---
+++ Bootstrap/webPleb.js
@@ -0,0 +1,8 @@
+function subscribe(){
+	document.getElementById("subscribe").reset();
+	alert("Thank you for subscribing!");
+}
+function clearForm(){
+	document.getElementById("myForm").reset;
+	alert("We appreciate your input!")
+}
